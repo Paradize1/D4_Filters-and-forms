c0c8723324ee059bbef39ff27785a3b523d83a74
Paradize1 <andrejsevcov946@gmail.com>
2023-07-07 20:57:37 +0300

    Фильтры

---
+++ project/simpleapp/views.py
@@ -0,0 +1,95 @@
+from django.urls import reverse_lazy
+from datetime import datetime
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
+from .models import Product, News
+from .filters import ProductFilter, NewsFilter
+from .forms import ProductForm, NewsForm
+
+class ProductsList(ListView):
+    model = Product
+    ordering = 'name'
+    template_name = 'products.html'
+    context_object_name = 'products'
+    paginate_by = 2  # вот так мы можем указать количество записей на странице
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = ProductFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Добавляем в контекст объект фильтрации.
+        context['filterset'] = self.filterset
+        return context
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'product.html'
+    context_object_name = 'product'
+
+
+# Добавляем новое представление для создания товаров.
+class ProductCreate(CreateView):
+    # Указываем нашу разработанную форму
+    form_class = ProductForm
+    # модель товаров
+    model = Product
+    # и новый шаблон, в котором используется форма.
+    template_name = 'product_edit.html'
+
+class ProductUpdate(UpdateView):
+    form_class = ProductForm
+    model = Product
+    template_name = 'product_edit.html'
+
+class ProductDelete(DeleteView):
+    model = Product
+    template_name = 'product_delete.html'
+    success_url = reverse_lazy('product_list')
+
+class NewsList(ListView):
+    model = News
+    ordering = 'title'
+    template_name = 'news.html'
+    context_object_name = 'news'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = NewsFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['filterset'] = self.filterset
+        return context
+
+class NewsDetail(DetailView):
+    model = News
+    template_name = 'news_number.html'
+    context_object_name = 'news_number'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+
+        return context
+
+class NewsCreate(CreateView):
+    form_class = NewsForm
+    model = News
+    template_name = 'news_create.html'
+
+class NewsUpdate(UpdateView):
+    form_class = NewsForm
+    model = News
+    template_name = 'News_edit.html'
+
+class NewsDelete(DeleteView):
+    model = News
+    template_name = 'news_delete.html'
+    success_url = reverse_lazy('news_list')
